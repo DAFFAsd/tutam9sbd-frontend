e2b6a4aa5c8f1987335b1b2230472581e00c6ee7
LordDaffaSF <daffa.sayra.firdaus@gmail.com>
2025-05-04 15:15:00 +0700

    jitsfuked

--- src/components/AddEmployeePage.jsx
+++ src/components/AddEmployeePage.jsx
@@ -25,7 +25,7 @@ export default function AddEmployeePage() {
     }
   
     try {
-      const response = await axios.post(`${import.meta.env.VITE_API_URL}employee/add`, {
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/employee/add`, {
         name,
         division,
         salary,
--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -30,7 +30,7 @@ const RegisterPage = () => {
         }
     
         try {
-            const response = await axios.post(`${import.meta.env.VITE_API_URL}manager/register`, {
+            const response = await axios.post(`${import.meta.env.VITE_API_URL}/manager/register`, {
                 email,
                 username,
                 password,
